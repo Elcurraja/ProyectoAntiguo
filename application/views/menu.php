6afe604df5a3971292768add12165293ea4c7c49
Elcurraja <33734068+Elcurraja@users.noreply.github.com>
2018-10-30 00:19:07 +0100

    Actualizacion de archivos base

---
+++ application/views/menu.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+   
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+    <title>Portal Administracion</title>
+</head>
+<body>
+    <div id="cont" class="sticky-top">
+        <nav class="menu navbar navbar-expand navbar-primary">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                    <li class="menu_li nav-item">
+                        <a class="nav-link" href="#">Principal</a>
+                        <div id="divlow1"></div>
+                    </li>
+                    <li class="menu_li nav-item">
+                        <a class="nav-link" href="#">Productos</a>
+                        <div id="divlow2"></div>
+                    </li>
+                    <li class="menu_li nav-item">
+                        <a class="nav-link" href="#">Pedidos</a>
+                        <div id="divlow2"></div>
+                    </li>
+                    <li class="menu_li nav-item">
+                        <a class="nav-link" href="#">Historico Almacen</a>
+                        <div id="divlow2"></div>
+                    </li>
+                    <li class="menu_li nav-item dropdown ">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Administracion
+                        </a>
+                        <div id="divlow4"></div>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Empleados</a>
+                            <a class="dropdown-item" href="#">Proveedores</a>
+                            <a class="dropdown-item" href="#">Clientes</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+ 
